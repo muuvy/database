7b56ab5f565c652f21f017f7b508de963ef6cab0
Luca Hostettler <luca.hostettler@iseag.ch>
2019-06-21 14:05:54 +0200

    Update DB Config

--- mongo-init.js
+++ mongo-init.js
@@ -1,17 +1,10 @@
 db.createUser({
     user: "muuvy",
     pwd: "muuvy",
-    mechanism: "SCRAM-SHA-1",
     roles: [{
             role: "readWrite",
             db: "muuvy"
         },
         "dbAdmin"
     ]
-});
-
-db = db.getSiblingDB('muuvy');
-
-db.foo.insert({
-    "key": "value"
 });
